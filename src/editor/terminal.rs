a67f19a5cc3fa0059da9cab94cdd8a35af756069
kawarimidoll <kawarimidoll+git@gmail.com>
2024-09-24 20:12:19 +1000

    refactor: add queue_command function

--- src/editor/terminal.rs
+++ src/editor/terminal.rs
@@ -1,27 +1,28 @@
 use crossterm::cursor::MoveTo;
-use crossterm::queue;
 use crossterm::terminal::{disable_raw_mode, enable_raw_mode, Clear, ClearType};
-use std::io::stdout;
+use crossterm::{queue, Command};
+use std::io::{stdout, Error};
 
 pub struct Terminal {}
 
 impl Terminal {
-    pub fn initialize() -> Result<(), std::io::Error> {
+    pub fn initialize() -> Result<(), Error> {
         enable_raw_mode()?;
         Self::clear_screen()?;
         Self::move_cursor_to(0, 0)?;
         Ok(())
     }
-    pub fn terminate() -> Result<(), std::io::Error> {
+    pub fn terminate() -> Result<(), Error> {
         disable_raw_mode()?;
         Ok(())
     }
-    pub fn clear_screen() -> Result<(), std::io::Error> {
-        queue!(stdout(), Clear(ClearType::All))?;
-        Ok(())
+    pub fn clear_screen() -> Result<(), Error> {
+        Self::queue_command(Clear(ClearType::All))
     }
-    pub fn move_cursor_to(x: u16, y: u16) -> Result<(), std::io::Error> {
-        queue!(stdout(), MoveTo(x, y))?;
-        Ok(())
+    pub fn move_cursor_to(x: u16, y: u16) -> Result<(), Error> {
+        Self::queue_command(MoveTo(x, y))
+    }
+    fn queue_command<T: Command>(command: T) -> Result<(), Error> {
+        queue!(stdout(), command)
     }
 }
