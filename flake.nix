9c6fd17638d183d55ca001ec30d768d7c8a39ea1
kawarimidoll <kawarimidoll+git@gmail.com>
2024-09-24 15:39:31 +1000

    chore: setup develop environment

---
+++ flake.nix
@@ -0,0 +1,127 @@
+{
+  description = "koi development environment";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+    }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+        in
+        {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              cargo
+              rustfmt
+            ];
+            shellHook = ''
+              echo "cargo"
+              which cargo
+              cargo --version
+            '';
+          };
+        }
+      );
+
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+        in
+        {
+          default = pkgs.stdenv.mkDerivation {
+            name = "koi";
+            src = ./.;
+            buildInputs = with pkgs; [ cargo ];
+            buildPhase = ''
+              cargo build
+            '';
+            installPhase = ''
+              install -D -t $out/bin target/debug/hecto
+            '';
+          };
+        }
+      );
+
+      apps = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+        in
+        {
+          default = {
+            type = "app";
+            buildInputs = with pkgs; [ cargo ];
+            program = toString (
+              pkgs.writeShellScript "cargo-run" ''
+                cargo run -- "$@"
+              ''
+            );
+          };
+          cargo = {
+            type = "app";
+            buildInputs = with pkgs; [ cargo ];
+            program = toString (
+              pkgs.writeShellScript "cargo-general" ''
+                cargo "$@"
+              ''
+            );
+          };
+          test = {
+            type = "app";
+            buildInputs = with pkgs; [ cargo ];
+            program = toString (
+              pkgs.writeShellScript "cargo-test" ''
+                cargo test
+              ''
+            );
+          };
+          clippy = {
+            type = "app";
+            buildInputs = with pkgs; [
+              cargo
+              cargo-clippy
+            ];
+            program = toString (
+              pkgs.writeShellScript "cargo-clippy" ''
+                cargo clippy
+              ''
+            );
+          };
+          fix = {
+            type = "app";
+            buildInputs = with pkgs; [
+              cargo
+              cargo-clippy
+            ];
+            program = toString (
+              pkgs.writeShellScript "cargo-fix" ''
+                cargo clippy --allow-dirty --fix --bin "koi"
+              ''
+            );
+          };
+        }
+      );
+    };
+}
