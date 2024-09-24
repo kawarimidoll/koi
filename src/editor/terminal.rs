f109954ed9be44e6d2e494de82a62909221b8686
kawarimidoll <kawarimidoll+git@gmail.com>
2024-09-24 20:25:53 +1000

    feat: add struct Position

--- src/editor/terminal.rs
+++ src/editor/terminal.rs
@@ -3,13 +3,19 @@ use crossterm::terminal::{disable_raw_mode, enable_raw_mode, Clear, ClearType};
 use crossterm::{queue, Command};
 use std::io::{stdout, Error};
 
+#[derive(Default)]
+pub struct Position {
+    pub row: usize,
+    pub col: usize,
+}
+
 pub struct Terminal {}
 
 impl Terminal {
     pub fn initialize() -> Result<(), Error> {
         enable_raw_mode()?;
         Self::clear_screen()?;
-        Self::move_cursor_to(0, 0)?;
+        Self::move_caret_to(Position::default())?;
         Ok(())
     }
     pub fn terminate() -> Result<(), Error> {
@@ -19,8 +25,8 @@ impl Terminal {
     pub fn clear_screen() -> Result<(), Error> {
         Self::queue_command(Clear(ClearType::All))
     }
-    pub fn move_cursor_to(x: u16, y: u16) -> Result<(), Error> {
-        Self::queue_command(MoveTo(x, y))
+    pub fn move_caret_to(position: Position) -> Result<(), Error> {
+        Self::queue_command(MoveTo(position.col as u16, position.row as u16))
     }
     fn queue_command<T: Command>(command: T) -> Result<(), Error> {
         queue!(stdout(), command)
