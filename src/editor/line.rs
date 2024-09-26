3ea1afda6bed69a622d456d8cdec361ac91d75a4
kawarimidoll <kawarimidoll+git@gmail.com>
2024-09-27 09:16:17 +1000

    feat: add get_fragment_by_byte_idx

--- src/editor/line.rs
+++ src/editor/line.rs
@@ -29,6 +29,16 @@ impl Line {
     pub fn grapheme_count(&self) -> usize {
         self.fragments.len()
     }
+    pub fn get_fragment_by_byte_idx(&self, byte_idx: usize) -> Option<&TextFragment> {
+        let mut acc = 0;
+        for fragment in self.fragments.iter() {
+            acc += fragment.width;
+            if acc > byte_idx {
+                return Some(fragment);
+            }
+        }
+        None
+    }
     pub fn grapheme_idx_to_byte_idx(&self, grapheme_idx: usize) -> usize {
         self.fragments
             .iter()
@@ -63,10 +73,26 @@ mod tests {
         assert_eq!(line.content(), "test_from");
         assert_eq!(line.grapheme_count(), 9);
         assert_eq!(line.grapheme_idx_to_byte_idx(5), 5);
+        assert_eq!(
+            line.get_fragment_by_byte_idx(4).map_or("", |f| &f.grapheme),
+            "_"
+        );
+        assert_eq!(
+            line.get_fragment_by_byte_idx(5).map_or("", |f| &f.grapheme),
+            "f"
+        );
 
         let line = Line::from("こんにちは");
         assert_eq!(line.content(), "こんにちは");
         assert_eq!(line.grapheme_count(), 5);
         assert_eq!(line.grapheme_idx_to_byte_idx(2), 4);
+        assert_eq!(
+            line.get_fragment_by_byte_idx(4).map_or("", |f| &f.grapheme),
+            "に"
+        );
+        assert_eq!(
+            line.get_fragment_by_byte_idx(5).map_or("", |f| &f.grapheme),
+            "に"
+        );
     }
 }
