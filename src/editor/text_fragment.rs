61f194a9c416a88c73d74f50d781e982ded751b8
kawarimidoll <kawarimidoll+git@gmail.com>
2024-09-26 16:22:32 +1000

    feat: calculate grapheme width

--- src/editor/text_fragment.rs
+++ src/editor/text_fragment.rs
@@ -1,11 +1,34 @@
+use unicode_width::UnicodeWidthStr;
+
 pub struct TextFragment {
     pub grapheme: String,
+    pub width: usize,
 }
 
 impl TextFragment {
     pub fn new(grapheme: &str) -> Self {
+        let width = if grapheme.width() <= 1 { 1 } else { 2 };
         Self {
             grapheme: String::from(grapheme),
+            width,
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_new() {
+        // normal character
+        let f = TextFragment::new("a");
+        assert_eq!(f.grapheme, "a");
+        assert_eq!(f.width, 1);
+
+        // full-width character
+        let f = TextFragment::new("緑");
+        assert_eq!(f.grapheme, "緑");
+        assert_eq!(f.width, 2);
     }
 }
