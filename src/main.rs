4b939e5048871b70fbe764272af8140b0a3fe7f4
kawarimidoll <kawarimidoll+git@gmail.com>
2024-10-14 10:27:13 +1100

    chore: add keycode checker as a comment

--- src/main.rs
+++ src/main.rs
@@ -13,3 +13,22 @@ use editor::Editor;
 fn main() {
     Editor::new().unwrap().run();
 }
+
+// // keycode checker
+// use crossterm::event::{read, Event, KeyCode};
+// use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
+// fn main() {
+//     enable_raw_mode().unwrap();
+//     loop {
+//         match read() {
+//             Ok(Event::Key(key_event)) => {
+//                 println!("{:?}\r", key_event);
+//                 if key_event.code == KeyCode::Char('q') {
+//                     break;
+//                 }
+//             }
+//             _ => {}
+//         }
+//     }
+//     disable_raw_mode().unwrap();
+// }
